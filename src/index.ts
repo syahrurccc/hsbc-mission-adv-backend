6fb838a19db2879d6841eb3e678fa4abc97d8e49
kizuna <syahrur.ramadhan94@gmail.com>
2025-12-19 17:03:35 +0800

    added /upload endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 
+import authRouter from "./routes/auth.route";
 import movieRouter from "./routes/movie.route";
+import uploadRouter from "./routes/upload.route";
 import { notFound } from "./middlewares/notFound";
 import { errorHandler } from "./middlewares/error";
 import * as db from "./db/db"
@@ -9,15 +11,17 @@ import * as db from "./db/db"
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-app.use(express.json());
 app.use(morgan("dev"));
+app.use("/upload", uploadRouter);
+
+app.use(express.json());
 
 app.get("/", async (_req, res) => {
   const { rows } = await db.query("SELECT NOW()")
   res.json(rows[0]);
 });
 
-
+app.use("/auth", authRouter);
 app.use("/movie", movieRouter);
 
 app.use(notFound);
---
+++ src/routes/upload.route.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import path from "node:path";
+import fs from "node:fs"
+import multer from "multer";
+
+import { throwErr } from "../utils/utils";
+
+const router = Router();
+
+const filesDir = path.resolve("upload");
+fs.mkdirSync(filesDir, { recursive: true });
+
+const storage = multer.diskStorage({
+  destination: (_req: any, _file: any, cb: any) => {
+    cb(null, filesDir);
+  },
+  filename: (_req: any, file: any, cb: any) => {
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/", upload.single("poster"), async (req, res) => {
+  if (!req.file) throwErr("No file uploaded", 400);
+  res.status(201).json(req.file);
+});
+
+export default router;
